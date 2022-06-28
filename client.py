b892e1b33c2d5358a2a4f8530643b349645bc13b
Ana Díaz Muñoz <58432799+aniitadiazm@users.noreply.github.com>
2022-06-28 17:00:51 +0200

    Versión 30

--- client.py
+++ client.py
@@ -6,14 +6,9 @@ import Ice
 Ice.loadSlice('IceFlix.ice')
 import IceFlix
 
-from Authenticator import Revocations
-import threading
-import getpass
+from authentication import Revocations
+
 import IceStorm
-import hashlib
-import time
-import logging
-import random
 
 
 USER = ""
@@ -22,7 +17,7 @@ PASSWORD_HASH = 'password_hash'
 DEFAULT_TOPICMANAGER_PROXY = 'IceStorm/TopicManager:tcp -p 10000'
 
 
-class Client():
+class Cliente():
     
     def __init__(self, token, password_hash, user):
         self.token = token
@@ -32,7 +27,8 @@ class Client():
 class Client(Ice.Application):
 
     def get_topic_manager(self):
-        proxy=self.communicator().stringToProxy(DEFAULT_TOPICMANAGER_PROXY)
+        
+        proxy = self.communicator().stringToProxy(DEFAULT_TOPICMANAGER_PROXY)
         if proxy is None:
             print("property {} not set".format(DEFAULT_TOPICMANAGER_PROXY))
             return None
@@ -41,49 +37,58 @@ class Client(Ice.Application):
         return IceStorm.TopicManagerPrx.checkedCast(proxy)
 
     def connectRevocations(self,adapter,topicManager,main_Server):
-        servant_Revocations=Revocations()
-        client_Subscriber=adapter.addWithUUID(servant_Revocations)
+        
+        servant_Revocations = Revocations()
+        client_Subscriber = adapter.addWithUUID(servant_Revocations)
 
-        topicRe="Revocations"
+        topicRe = "Revocations"
+        
         try:
-            topic_Revocations=topicManager.retrieve(topicRe)
+            topic_Revocations = topicManager.retrieve(topicRe)
+            
         except IceStorm.NoSuchTopic:
             print("No such topic")
             topic_Revocations=topicManager.create(topicRe)
         
         topic_Revocations.subscribeAndGetPublisher({},client_Subscriber)
-        servant_Revocations.mainService=main_Server
+        servant_Revocations.mainService = main_Server
+        
         return servant_Revocations
     
     def subscriberClient(self,adapter,topicManager,sincTopic,main_Server):
-        servant_StreamSync=StreamSync()
-        client_Subscriber=adapter.addWithUUID(servant_StreamSync)
+        
+        servant_StreamSync = StreamSync()
+        client_Subscriber = adapter.addWithUUID(servant_StreamSync)
 
-        topicRe=sincTopic
+        topicRe = sincTopic
+        
         try:
             topic_Revocations=topicManager.retrieve(topicRe)
+            
         except IceStorm.NoSuchTopic:
             print("No such topic")
             topic_Revocations=topicManager.create(topicRe)
         
         topic_Revocations.subscribeAndGetPublisher({},client_Subscriber)
-        servant_StreamSync.mainService=main_Server
+        servant_StreamSync.mainService = main_Server
+        
         return servant_StreamSync
         
     def run(self, argv):
 
         broker = self.communicator()
-        topicManager=self.get_topic_manager()
+        topicManager = self.get_topic_manager()
         adapter = broker.createObjectAdapterWithEndpoints("IceFlixAdapter","tcp")
-        servant_Revocations=Revocations()
-        client_Subscriber=adapter.addWithUUID(servant_Revocations)
-
-        topicRe="Revocations"
+        servant_Revocations = Revocations()
+        client_Subscriber = adapter.addWithUUID(servant_Revocations)
+        topicRe = "Revocations"
+        
         try:
-            topic_Revocations=topicManager.retrieve(topicRe)
+            topic_Revocations = topicManager.retrieve(topicRe)
+            
         except IceStorm.NoSuchTopic:
             print("No such topic")
-            topic_Revocations=topicManager.create(topicRe)
+            topic_Revocations = topicManager.create(topicRe)
         
         topic_Revocations.subscribeAndGetPublisher({},client_Subscriber)
         
